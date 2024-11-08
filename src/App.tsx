9a297c2a25d2cbb1a0c8607c6bf26de5e50617be
Marcin Radoszewski <moriturius@gmail.com>
2024-11-08 13:31:48 +0100

    Remove AppController

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,8 @@ import { Search } from "./search/Search";
 import { Action } from "./core/Action";
 import { SearchController } from "./search/SearchController";
 
-class AppController {
-  notes: Note[];
-  setNotes: (notes: Note[]) => void;
-  selectedIndex: number = 0;
-  setSelectedIndex: (index: number) => void;
-
-  constructor(initialNotes: Note[]) {
-    const [notes, setNotes] = useState<Array<Note>>(initialNotes);
-    this.notes = notes;
-    this.setNotes = setNotes;
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    this.selectedIndex = selectedIndex;
-    this.setSelectedIndex = setSelectedIndex;
-  }
-
-  updateNoteContent(index: number, content: string) {
-    this.setNotes(this.notes.map((note, idx) => {
-      if (idx == index) {
-        return new Note(note.name, content);
-      } else {
-        return note
-      }
-    }))
-  }
-}
-
 function App() {
-  const ctl = new AppController([
+  const [notes, setNotes] = useState<Array<Note>>([
     new Note(
       "test.note",
       "Hello World!"
@@ -43,10 +17,22 @@ function App() {
       "That is another one"
     )
   ]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const searchCtl = new SearchController();
   // const [searchResults, setSearchResults] = useState<Action[]>([]);
   // const [searchVisible, setSearchVisible] = useState(false);
+
+
+  function updateNoteContent(index: number, content: string) {
+    setNotes(notes.map((note, idx) => {
+      if (idx == index) {
+        return new Note(note.name, content);
+      } else {
+        return note
+      }
+    }))
+  }
 
   const actions = [
     new Action("First action", "description", () => { console.log("First action") }),
@@ -56,16 +42,16 @@ function App() {
 
   return (
     <main className="container mx-auto">
-      <Stream 
-        notes={ctl.notes} 
-        onUpdateNote={(idx,content) => ctl.updateNoteContent(idx,content)}
-        />
+      <Stream
+        notes={notes}
+        onUpdateNote={(idx, content) => updateNoteContent(idx, content)}
+      />
 
-        <Search controller={searchCtl} />
+      <Search controller={searchCtl} />
       <button onClick={() => searchCtl.startActionSearch(actions)}>Search actions</button>
       <button onClick={() => searchCtl.startYestNo(
-        "Are you sure?", 
-        () => { console.log("Yes") }, 
+        "Are you sure?",
+        () => { console.log("Yes") },
         () => { console.log("No") }
       )}>Yes/No</button>
       <button onClick={() => searchCtl.startInput("What's your age?", (age) => { console.log("Your age is", age) })}>Get input</button>
